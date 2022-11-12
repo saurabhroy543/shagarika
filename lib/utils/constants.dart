32ef26f77b96080f12626853f8b22eb9ba1f56db
shubham <shubham.saurabh@plutonic.co.in>
2022-11-13 01:39:11 +0530

    base class added.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,10 @@ class Timeouts {
   static const connectTimeout = 15000; // 15 seconds
   static const receiveTimeout = 15000; // 15 seconds
 }
+class BaseUrl{
+  static const baseurl = 'https://api.shagarika.com';
 
+}
 class ErrorMessages {
   static const noInternet = 'Please check your internet connection';
   static const connectionTimeout = 'Please check your internet connection';
