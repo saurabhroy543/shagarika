bae4a85f00080bd0dbc77164cb5a42a7ee5408ad
shubham <shubham.saurabh@plutonic.co.in>
2023-05-14 11:54:19 +0530

    -Purchased scheme url added.

--- lib/utils/api_paths.dart
+++ lib/utils/api_paths.dart
@@ -3,8 +3,9 @@ class ApiPaths{
   static const String amcDetail = '/ws-get-amc-data/';
   static const String amcList = '/getamc/';
   static const String schemeList = '/getscheme/';
-  static const String folio = '/getfolio/';
+  static const String folio = '/getfolioscheme/';
   static const String userDetail = '/ws-get-user-detail/';
   static const String encrpytData = '/encryptdata/';
-  static const String sendMail = '//sendemail/';
+  static const String sendMail = '/sendemail/';
+  static const String purchasedScheme = '/getschemebypanamc/';
 }
